a887efd8cda87806e090633d4172828235abfe08
GabrielTarpian <gabriel.tarpian93@gmail.com>
2016-11-28 17:10:24 +0200

    Display quote

--- MotivationFromSteveJobs/ViewController.h
+++ MotivationFromSteveJobs/ViewController.h
@@ -7,9 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DBManager.h"
 
 @interface ViewController : UIViewController
 
+@property (nonatomic,retain) IBOutlet UITextView *textQuote;
 
 @end
 
--- MotivationFromSteveJobs/ViewController.m
+++ MotivationFromSteveJobs/ViewController.m
@@ -23,6 +23,13 @@
     int width = screenRect.size.width;
     int height = screenRect.size.height;
     
+    // creating the quote text label
+    int labelPosX = width / 3;
+    int labelPosY = height / 7;
+    int labelWidth = width - labelPosX;
+    int labelHeight = height - labelPosY *2.5;
+    _textQuote = [[UITextView alloc]initWithFrame:CGRectMake(labelPosX, labelPosY,labelWidth,labelHeight)];
+    
     // Create wallpaper
     UIImageView *imageHolder = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, width, height)];
     UIImage *image = [UIImage imageNamed:@"Wallpaper"];
@@ -35,9 +42,36 @@
     NSDateComponents *components = [calendar components:NSCalendarUnitDay fromDate:date];
     int day = (int) (components.day);
     
+    // get quote of the day
+    NSString *quote = [[DBManager getSharedInstance] findById:day];
+    
     NSLog(@"Current day is %d\n",day);
+    NSLog(@"Quote of the day is: %@\n",quote);
+    
+    // create a font
+    UIFont *textViewfont = [UIFont fontWithName:@"Courier" size:20];
+    
+    // Assign quote to label
+    _textQuote.text = quote;
+    _textQuote.textColor = [UIColor whiteColor];
+    _textQuote.backgroundColor = [UIColor blackColor];
+    [_textQuote setUserInteractionEnabled:NO];
+    _textQuote.font = textViewfont;
+    _textQuote.textAlignment = NSTextAlignmentCenter;
+    
+    // Vertical center alignment
+    [self adjustContentSize:_textQuote];
+    
+    [self.view addSubview:_textQuote];
+    
 }
 
+// Function that vertically aligns the text view
+-(void)adjustContentSize:(UITextView*)tv{
+    CGFloat deadSpace = ([tv bounds].size.height - [tv contentSize].height);
+    CGFloat inset = MAX(0, deadSpace/2.0);
+    tv.contentInset = UIEdgeInsetsMake(inset, tv.contentInset.left, inset, tv.contentInset.right);
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
